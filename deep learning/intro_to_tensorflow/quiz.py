2ff31de28e670f0c157455ae56d10acfa74a918e
yacin hamdi <117452150+yacin-hamdi@users.noreply.github.com>
2023-07-09 21:05:45 +0100

    into to tf

---
+++ deep learning/intro_to_tensorflow/quiz.py
@@ -0,0 +1,57 @@
+# Solution is available in the other "quiz_solution.py" tab
+import tensorflow as tf
+import math
+
+
+def get_weights(n_features, n_labels):
+    """
+    Return TensorFlow weights
+    :param n_features: Number of features
+    :param n_labels: Number of labels
+    :return: TensorFlow weights
+    """
+    # TODO: Return weights
+    return tf.Variable(tf.truncated_normal((n_features, n_labels)))
+
+
+def get_biases(n_labels):
+    """
+    Return TensorFlow bias
+    :param n_labels: Number of labels
+    :return: TensorFlow bias
+    """
+    # TODO: Return biases
+    return tf.Variable(tf.zeros(n_labels))
+
+
+def linear(input, w, b):
+    """
+    Return linear function in TensorFlow
+    :param input: TensorFlow input
+    :param w: TensorFlow weights
+    :param b: TensorFlow biases
+    :return: TensorFlow linear function
+    """
+    # TODO: Linear Function (xW + b)
+    return tf.add(tf.matmul(input, w), b)
+
+
+def batches(batch_size, features, labels):
+    """
+    Create batches of features and labels
+    :param batch_size: The batch size
+    :param features: List of features
+    :param labels: List of labels
+    :return: Batches of (Features, Labels)
+    """
+    assert len(features) == len(labels)
+    # TODO: Implement batching
+    batchs = []
+    features_size = len(features)
+    
+    for i in range(0, features_size, batch_size):
+        end_i = i + batch_size
+        batchs.append([features[i:end_i], labels[i:end_i]])
+    
+    return batchs
+
