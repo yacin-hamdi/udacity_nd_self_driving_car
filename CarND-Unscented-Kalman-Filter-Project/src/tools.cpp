cd410d8eee2a1976f93950e629665e011cf30ea0
yacin hamdi <117452150+yacin-hamdi@users.noreply.github.com>
2023-09-06 14:57:34 +0100

    add rmse function

--- CarND-Unscented-Kalman-Filter-Project/src/tools.cpp
+++ CarND-Unscented-Kalman-Filter-Project/src/tools.cpp
@@ -1,5 +1,5 @@
 #include "tools.h"
-
+#include<iostream>
 using Eigen::VectorXd;
 using std::vector;
 
@@ -12,4 +12,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+
+    VectorXd rmse(4);
+    rmse.fill(0.0);
+    VectorXd temp(4);
+    temp.fill(0.0);
+    if (estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+        std::cout << "[-] error check the estimations or ground_truth vector" << std::endl;
+        return rmse;
+    }
+
+    for (int i = 0; i < estimations.size(); i++) {
+        temp = estimations[i] - ground_truth[i];
+        temp = temp.array().pow(2);
+        rmse += temp;
+    }
+
+    rmse = rmse / estimations.size();
+    rmse = rmse.array().sqrt();
+    return rmse;
 }
